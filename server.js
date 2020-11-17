35bd9c2cf8aa0526ec4706409782e8a8592cbb30
bjgman12 <bjgman38@gmail.com>
2020-11-17 12:13:18 -0700

    have data for charts still need to do rendering

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
-
 const express = require('express');
 const superagent = require('superagent');
 const cors = require('cors');
 const pg = require('pg');
 const methodOverride = require('method-override');
-
+const cheerio = require('cheerio');
+const chart = require('chart.js');
 // Environment variables
 require('dotenv').config();
 
@@ -22,7 +22,70 @@ app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
 
 
+//Routes
+app.get('/', homehandler);
+
+function homehandler(req, res) {
+  const API = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=3`;
+  const coin_market_cap = process.env.coin_market_cap;
+  superagent.get(API)
+    .set('X-CMC_PRO_API_KEY', coin_market_cap)
+    .then(data => {
+      let graphData = data.body.data.map(coin => new CMC(coin));
+      console.log(graphData);
+      let chartData = [];
+      graphData.forEach((coin) => {
+        let oldPrice = coin.price;
+        for (let i = 0; i < 7; i++) {
+          oldPrice -= (coin.price * (coin.weeklyChange / 100 / 7));
+          chartData.push(oldPrice);
+        }
+      });
+      console.log(chartData);
+      // let ctx = $('#cryptoOne');
+      // let myChart = new Chart(ctx, {
+      //   type: 'bar',
+      //   data: {
+      //     labels
+      //   }
+      // })
+      res.status(200).render('pages/index');
+    })
+    .catch((error) => console.log(error));
+}
+
+//Constructors
+
+function CMC(obj) {//CMC = coinMarketCap
+  this.name = obj.name;
+  this.symbol = obj.symbol;
+  this.price = obj.quote.USD.price;
+  this.dailyChange = obj.quote.USD.percent_change_24h;
+  this.weeklyChange = obj.quote.USD.percent_change_7d;
+}
+
 //Start Server
 client.connect()
   .then(() => app.listen(PORT, () => console.log(`Server now listening on port ${PORT}.`)))
   .catch(err => console.log('ERROR:', err));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
